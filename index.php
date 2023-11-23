080d538bdb6fcec3a7183c2af199b1fcd8c19af3
Sebastian Triana martinez <sebastian.triana@utp.edu.co>
2023-11-22 19:11:47 -0500

    cambios hechos

--- index.php
+++ index.php
@@ -8,42 +8,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://vjs.zencdn.net/8.6.1/video-js.css" rel="stylesheet" />
-    <style>
-        /* Estilos adicionales para hacer el reproductor más atractivo */
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #222;
-            color: #fff;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            height: 100vh;
-        }
-        #my-video {
-            width: 100%;
-            height: 100%;
-            max-height: 450px;
-            max-width: 800px;
-            margin: auto;
-        }
-        .player{
-            margin: auto;
-            align-self: center;
-            width: 100%;
-            max-width: 800px;
-            background-color: #fff;
-            height: calc(100vw * 9 / 16);
-            max-height: 450px;
-        }
-    </style>
+    <link rel="stylesheet" href="frontend/styles/index.css">
 </head>
 <body>
-    <div class="player">
-        <video id="my-video" class="video-js" controls preload="auto" width="640" height="360"
-            data-setup='{}'>
-            <source src="https://unoatv.com/nginx/streaming/livestream/livestream.m3u8" type="application/x-mpegURL">
-        </video>
-    </div>
+    <header>
+        <div>
+            <img src="assets/media/images/logo.png" alt="">
+        </div>
+    </header>
+    <main>
+        <div class="player">
+            <video id="my-video" class="video-js" controls preload="auto" width="640" height="360"
+                data-setup='{}'>
+                <source src="https://unoatv.com/nginx/streaming/livestream/livestream.m3u8" type="application/x-mpegURL">
+            </video>
+        </div>
+    </main>
     <script src="https://vjs.zencdn.net/8.6.1/video.min.js"></script>
 
 </body>
